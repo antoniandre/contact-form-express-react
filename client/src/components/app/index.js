188545441cccf3f2ab5b3d80de625a08101d00d6
disadb ** <disadb@gmail.com>
2017-09-08 20:46:49 +1000

    Add new routes to components home and contact.

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -3,6 +3,8 @@ import { Route, Link } from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Home from '../home'
+import Contact from '../contact'
 import Logo from './logo.svg'
 import './index.css'
 
@@ -12,13 +14,15 @@ const App = () => (
     // Make Material UI available everywhere.
     <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div className="app">
-            <div className="App-header">
-                <img src={Logo} className="app-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-            </div>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
+            <header>
+                <Link className="app-logo" to="/"><img src={Logo} alt="logo" /></Link>
+                <Link className="menu-link" to="/">Home</Link>
+                <Link className="menu-link" to="/contact-us">Contact</Link>
+            </header>
+            <main>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/contact-us" component={Contact} />
+            </main>
         </div>
     </MuiThemeProvider>
 )
