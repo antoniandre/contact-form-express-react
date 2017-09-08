189553c989af169a68b17e98e7d27dd85f3b32d5
disadb ** <disadb@gmail.com>
2017-09-08 21:14:21 +1000

    Add a thank you message on successful submission.

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -24,7 +24,6 @@ async function submitToServer(data) {
     }
 }
 
-
 const validate = values => {
     const errors = {}
     const requiredFields = [ 'firstName', 'lastName', 'email', 'message' ]
@@ -41,13 +40,35 @@ const validate = values => {
 }
 
 
+class Thankyou extends React.Component {
+
+    render = () => (
+        <div className="thankyou">
+            <div className="thankyou-inner">
+                <svg version="1.1" id="tick" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 37 37">
+                    <path className="svg-circ path" d="M30.5,6.5L30.5,6.5c6.6,6.6,6.6,17.4,0,24l0,0c-6.6,6.6-17.4,6.6-24,0l0,0c-6.6-6.6-6.6-17.4,0-24l0,0C13.1-0.2,23.9-0.2,30.5,6.5z"/>
+                    <polyline className="svg-tick path" points="11.6,20 15.9,24.2 26.4,13.8"/>
+                </svg>
+                <p>Thank you!<br/>{this.props.message}</p>
+            </div>
+        </div>
+    )
+}
+
 class ContactForm extends React.Component {
+
+    state = {
+        success: false,// Whether the form submission is successful or not.
+        message: ""// A message returned from the server.
+    }
 
     submit = values => {
         submitToServer(values).then(data => {
+            // If a message is returned show it in the thankyou container.
             if (data.message !== undefined) {
                 this.setState({success: true, message: data.message});
             }
+            // Here you can check the returned data from the server.
             console.log(data)
         })
     }
@@ -67,28 +88,32 @@ class ContactForm extends React.Component {
      * @memberof ContactForm
      */
     render = () => (
-        <div className="form-wrapper">
-            <form onSubmit={this.props.handleSubmit(this.submit)}>
-                <div>
-                    <Field name="firstName" component={this.renderTextField} label="First Name"/>
+            <div>
+                <h1>Contact Us</h1>
+                <div className="form-wrapper">
+                    <form onSubmit={this.props.handleSubmit(this.submit)}>
+                        <div>
+                            <Field name="firstName" component={this.renderTextField} label="First Name"/>
+                        </div>
+                        <div>
+                            <Field name="lastName" component={this.renderTextField} label="Last Name"/>
+                        </div>
+                        <div>
+                            <Field name="email" component={this.renderTextField} label="Email"/>
+                        </div>
+                        <div>
+                            <Field name="message" component={this.renderTextField} label="Message" multiLine={true} rows={2}/>
+                        </div>
+                        <div>
+                            {/* <button type="submit" disabled={pristine || submitting}>Submit</button> */}
+                            <RaisedButton label="Clear Values" disabled={this.props.pristine || this.props.submitting} onClick={this.props.reset}/> &nbsp;
+                            <RaisedButton label="submit" onClick={this.props.handleSubmit(this.submit)} primary disabled={this.props.pristine || this.props.submitting}/>
+                        </div>
+                    </form>
+                    <Thankyou message={this.state.message}/>
                 </div>
-                <div>
-                    <Field name="lastName" component={this.renderTextField} label="Last Name"/>
-                </div>
-                <div>
-                    <Field name="email" component={this.renderTextField} label="Email"/>
-                </div>
-                <div>
-                    <Field name="message" component={this.renderTextField} label="Message" multiLine={true} rows={2}/>
-                </div>
-                <div>
-                    {/* <button type="submit" disabled={pristine || submitting}>Submit</button> */}
-                    <RaisedButton label="Clear Values" disabled={this.props.pristine || this.props.submitting} onClick={this.props.reset}/> &nbsp;
-                    <RaisedButton label="submit" onClick={this.props.handleSubmit(this.submit)} primary disabled={this.props.pristine || this.props.submitting}/>
-                </div>
-            </form>
-        </div>
-    )
+            </div>
+        )
 }
 
 // Decorate the form component.
