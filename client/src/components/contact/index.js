75aaea037b72f9ac90a05a9bc6cc2c628bf9cfe9
disadb ** <disadb@gmail.com>
2017-09-08 21:19:01 +1000

    Add documentation. :)

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -2,13 +2,27 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+
+// This is just a simulation of server check for an existing email address on validate.
+// In the case of a contact form it is useless but I find this piece of code interesting as
+// another real life layer of validation.
 import asyncValidate from './async-validate';
 
 import './index.css';
 
 
+/**
+ * This is where the valid submitted data.
+ *
+ * @param {object} data: an object of data we want to pass to the server.
+ * @returns void.
+ */
 async function submitToServer(data) {
     try {
+        // The current react app is running on port 3000,
+        // and the backend server (Express) is set to run on port 3001 for no conflict.
+        // So we want to have a proxy set in client/package.json to transfer the request from :3000 to 3001.
+        // Otherwise there will be a cross-origin call error.
         let response = await fetch('/contact', {
             method: 'post',
             headers: {
@@ -24,6 +38,12 @@ async function submitToServer(data) {
     }
 }
 
+/**
+ * This is the validation function from Material UI.
+ * http://redux-form.com/6.1.1/examples/material-ui/
+ *
+ * @param {object} values: object containing all the form fields to validate.
+ */
 const validate = values => {
     const errors = {}
     const requiredFields = [ 'firstName', 'lastName', 'email', 'message' ]
@@ -39,7 +59,12 @@ const validate = values => {
     return errors
 }
 
-
+/**
+ * This component is used to render the thankyou message after a valid submission.
+ *
+ * @class Thankyou
+ * @extends {React.Component}
+ */
 class Thankyou extends React.Component {
 
     // For the 'valid' svg animation we need to delay the addition of the class 'show'.
@@ -68,6 +93,16 @@ class Thankyou extends React.Component {
     )
 }
 
+/**
+ * Main form class using redux form (http://redux-form.com/6.1.1/).
+ * it first renders the form using material ui design then attaches the validation.
+ * The material ui 'MuiThemeProvider' wrapper is set in app/index.js.
+ * When no error is found the form submits to the server through submitToServer() and display
+ * the thank you message from the Thankyou component.
+ *
+ * @class ContactForm
+ * @extends {React.Component}
+ */
 class ContactForm extends React.Component {
 
     state = {
@@ -75,6 +110,11 @@ class ContactForm extends React.Component {
         message: ""// A message returned from the server.
     }
 
+    /**
+     * Function called when the form is submitted.
+     *
+     * @memberof ContactForm
+     */
     submit = values => {
         submitToServer(values).then(data => {
             // If a message is returned show it in the thankyou container.
@@ -86,6 +126,11 @@ class ContactForm extends React.Component {
         })
     }
 
+    /**
+     * Helper to write material-ui valid fields.
+     *
+     * @memberof ContactForm
+     */
     renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
         <TextField hintText={label}
             floatingLabelText={label}
