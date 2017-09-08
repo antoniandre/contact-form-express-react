b78bd407b2c7a663630b7f6ece2d79c03c5739fe
disadb ** <disadb@gmail.com>
2017-09-08 20:33:37 +1000

    Rearchitecturing after adding redux + add store & reducers.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './components/app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+        <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
