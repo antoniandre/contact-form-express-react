77975787874f8b122afd72320b8f6b18b25370ec
disadb ** <disadb@gmail.com>
2017-09-08 20:42:21 +1000

    Add Material UI support + rename css file and classes.

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Logo from './logo.svg'
+import './index.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+injectTapEventPlugin();
+
+const App = () => (
+    // Make Material UI available everywhere.
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div className="app">
+            <div className="App-header">
+                <img src={Logo} className="app-logo" alt="logo" />
+                <h2>Welcome to React</h2>
+            </div>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    </MuiThemeProvider>
+)
 
-export default App;
+export default App
