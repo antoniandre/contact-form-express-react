5883ba7625495550b26495334902429e08251c07
disadb ** <disadb@gmail.com>
2017-09-08 20:57:31 +1000

    Add of material ui form from the official redux form site.

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import './index.css';
 
 
 class ContactForm extends React.Component {
@@ -7,12 +11,43 @@ class ContactForm extends React.Component {
     submit = values => {
     }
 
+    renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
+        <TextField hintText={label}
+            floatingLabelText={label}
+            errorText={touched && error}
+            {...input}
+            {...custom}
+        />
+    )
 
+    /**
+     * Main form render using Material UI.
+     *
+     * @memberof ContactForm
+     */
     render = () => (
-            <div>
-                <h1>Contact Us</h1>
-            </div>
-        )
+        <div className="form-wrapper">
+            <form onSubmit={this.props.handleSubmit(this.submit)}>
+                <div>
+                    <Field name="firstName" component={this.renderTextField} label="First Name"/>
+                </div>
+                <div>
+                    <Field name="lastName" component={this.renderTextField} label="Last Name"/>
+                </div>
+                <div>
+                    <Field name="email" component={this.renderTextField} label="Email"/>
+                </div>
+                <div>
+                    <Field name="message" component={this.renderTextField} label="Message" multiLine={true} rows={2}/>
+                </div>
+                <div>
+                    {/* <button type="submit" disabled={pristine || submitting}>Submit</button> */}
+                    <RaisedButton label="Clear Values" disabled={this.props.pristine || this.props.submitting} onClick={this.props.reset}/> &nbsp;
+                    <RaisedButton label="submit" onClick={this.props.handleSubmit(this.submit)} primary disabled={this.props.pristine || this.props.submitting}/>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 // Decorate the form component.
